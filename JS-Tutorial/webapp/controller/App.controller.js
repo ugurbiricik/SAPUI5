53a80a1e0e1d6bdcf7db861c4980f0d603928848
Ugur Biricik <biricik.ugurr@gmail.com>
2024-08-19 16:25:09 +0200

    Nested Views

--- JS-Tutorial/webapp/controller/App.controller.js
+++ JS-Tutorial/webapp/controller/App.controller.js
@@ -1,20 +1,5 @@
-sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  (Controller, MessageToast) => {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+  "use strict";
 
-    return Controller.extend("ui5.walkthrough.controller.App", {
-      onShowHello() {
-        // read msg from i18n model
-        const oBundle = this.getView().getModel("i18n").getResourceBundle();
-        const sRecipient = this.getView()
-          .getModel()
-          .getProperty("/recipient/name");
-        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-        // show message
-        MessageToast.show(sMsg);
-      },
-    });
-  }
-);
+  return Controller.extend("ui5.walkthrough.controller.App", {});
+});
