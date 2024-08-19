a921e60eb8e098a2722cc3bc2e0f2fa9154842c0
Ugur Biricik <biricik.ugurr@gmail.com>
2024-08-19 16:04:37 +0200

    controllers

---
+++ JS-Tutorial/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+  "use strict";
+
+  return Controller.extend("ui5.walkthrough.controller.App", {
+    onShowHello() {
+      // show a native JavaScript alert
+      alert("Hello World");
+    },
+  });
+});
