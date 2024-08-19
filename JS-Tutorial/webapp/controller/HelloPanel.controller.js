294c3e956c2d1a5aa90ce7ecbaf3aa79b9058311
Ugur Biricik <biricik.ugurr@gmail.com>
2024-08-19 16:30:30 +0200

    Fragment Callbacks

--- JS-Tutorial/webapp/controller/HelloPanel.controller.js
+++ JS-Tutorial/webapp/controller/HelloPanel.controller.js
@@ -24,6 +24,12 @@ sap.ui.define(
 
         this.oDialog.open();
       },
+
+      onCloseDialog() {
+        // note: We don't need to chain to the pDialog promise, since this event handler
+        // is only called from within the loaded dialog itself.
+        this.byId("helloDialog").close();
+      },
     });
   }
 );
