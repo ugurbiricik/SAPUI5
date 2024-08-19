30735939ba15828641b2231e32844f4157f4f4eb
Ugur Biricik <biricik.ugurr@gmail.com>
2024-08-19 21:46:14 +0200

    Aggregation Binding Using a Factory Function

--- Data-Binding/webapp/controller/App.controller.js
+++ Data-Binding/webapp/controller/App.controller.js
@@ -1,6 +1,11 @@
 sap.ui.define(
-  ["sap/m/library", "sap/ui/core/mvc/Controller", "sap/ui/model/type/Currency"],
-  (mobileLibrary, Controller, Currency) => {
+  [
+    "sap/m/library",
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/type/Currency",
+    "sap/m/ObjectAttribute",
+  ],
+  (mobileLibrary, Controller, Currency, ObjectAttribute) => {
     "use strict";
 
     return Controller.extend("ui5.databinding.controller.App", {
@@ -28,6 +33,34 @@ sap.ui.define(
         const sPath = oContext.getPath();
         const oProductDetailPanel = this.byId("productDetailsPanel");
         oProductDetailPanel.bindElement({ path: sPath, model: "products" });
+      },
+      productListFactory(sId, oContext) {
+        let oUIControl;
+
+        // Decide based on the data which dependent to clone
+        if (
+          oContext.getProperty("UnitsInStock") === 0 &&
+          oContext.getProperty("Discontinued")
+        ) {
+          // The item is discontinued, so use a StandardListItem
+          oUIControl = this.byId("productSimple").clone(sId);
+        } else {
+          // The item is available, so we will create an ObjectListItem
+          oUIControl = this.byId("productExtended").clone(sId);
+
+          // The item is temporarily out of stock, so we will add a status
+          if (oContext.getProperty("UnitsInStock") < 1) {
+            oUIControl.addAttribute(
+              new ObjectAttribute({
+                text: {
+                  path: "i18n>outOfStock",
+                },
+              })
+            );
+          }
+        }
+
+        return oUIControl;
       },
     });
   }
