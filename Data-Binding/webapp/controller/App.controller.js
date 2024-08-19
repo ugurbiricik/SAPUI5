0d5f91e09365087fcb31cf9e91b13f882f7dae6e
Ugur Biricik <biricik.ugurr@gmail.com>
2024-08-19 21:33:20 +0200

    Aggretion Binding Using Templates

--- Data-Binding/webapp/controller/App.controller.js
+++ Data-Binding/webapp/controller/App.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
-  ["sap/m/library", "sap/ui/core/mvc/Controller"],
-  (mobileLibrary, Controller) => {
+  ["sap/m/library", "sap/ui/core/mvc/Controller", "sap/ui/model/type/Currency"],
+  (mobileLibrary, Controller, Currency) => {
     "use strict";
 
     return Controller.extend("ui5.databinding.controller.App", {
@@ -8,9 +8,18 @@ sap.ui.define(
         const oBundle = this.getView().getModel("i18n").getResourceBundle();
 
         return mobileLibrary.URLHelper.normalizeEmail(
-          `${sFirstName}.${sLastName}@example.com`,
+          sFirstName + "." + sLastName + "@example.com",
           oBundle.getText("mailSubject", [sFirstName]),
           oBundle.getText("mailBody")
+        );
+      },
+
+      formatStockValue(fUnitPrice, iStockLevel, sCurrCode) {
+        const oCurrency = new Currency();
+
+        return oCurrency.formatValue(
+          [fUnitPrice * iStockLevel, sCurrCode],
+          "string"
         );
       },
     });
