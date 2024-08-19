86b33c686b1d86af5d95426f28987dcf9af69877
Ugur Biricik <biricik.ugurr@gmail.com>
2024-08-19 17:10:27 +0200

    Routing Back and History

--- JS-Tutorial/webapp/controller/Detail.controller.js
+++ JS-Tutorial/webapp/controller/Detail.controller.js
@@ -1,23 +1,38 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
+  (Controller, History) => {
+    "use strict";
 
-  return Controller.extend("ui5.walkthrough.controller.Detail", {
-    onInit() {
-      const oRouter = this.getOwnerComponent().getRouter();
-      oRouter
-        .getRoute("detail")
-        .attachPatternMatched(this.onObjectMatched, this);
-    },
+    return Controller.extend("ui5.walkthrough.controller.Detail", {
+      onInit() {
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter
+          .getRoute("detail")
+          .attachPatternMatched(this.onObjectMatched, this);
+      },
 
-    onObjectMatched(oEvent) {
-      this.getView().bindElement({
-        path:
-          "/" +
-          window.decodeURIComponent(
-            oEvent.getParameter("arguments").invoicePath
-          ),
-        model: "invoice",
-      });
-    },
-  });
-});
+      onObjectMatched(oEvent) {
+        this.getView().bindElement({
+          path:
+            "/" +
+            window.decodeURIComponent(
+              oEvent.getParameter("arguments").invoicePath
+            ),
+          model: "invoice",
+        });
+      },
+
+      onNavBack() {
+        const oHistory = History.getInstance();
+        const sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("overview", {}, true);
+        }
+      },
+    });
+  }
+);
