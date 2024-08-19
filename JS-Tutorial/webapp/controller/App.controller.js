d1e8c16fc225c68a7b38defe6402bc2f9fc37a45
Ugur Biricik <biricik.ugurr@gmail.com>
2024-08-19 16:10:59 +0200

    Translatable Texts

--- JS-Tutorial/webapp/controller/App.controller.js
+++ JS-Tutorial/webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  (Controller, MessageToast, JSONModel) => {
+  (Controller, MessageToast, JSONModel, ResourceModel) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App", {
@@ -17,10 +18,24 @@ sap.ui.define(
         };
         const oModel = new JSONModel(oData);
         this.getView().setModel(oModel);
+
+        // set i18n model on view
+        const i18nModel = new ResourceModel({
+          bundleName: "ui5.walkthrough.i18n.i18n",
+        });
+        this.getView().setModel(i18nModel, "i18n");
       },
 
       onShowHello() {
-        MessageToast.show("Hello World");
+        // read msg from i18n model
+        const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        const sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+        // show message
+        MessageToast.show(sMsg);
       },
     });
   }
