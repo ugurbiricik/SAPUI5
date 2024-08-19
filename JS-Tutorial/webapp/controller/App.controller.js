98cb363548612e96febdb4a9655b8df20d045479
Ugur Biricik <biricik.ugurr@gmail.com>
2024-08-19 16:05:28 +0200

    Modules (MessageToast)

--- JS-Tutorial/webapp/controller/App.controller.js
+++ JS-Tutorial/webapp/controller/App.controller.js
@@ -1,10 +1,12 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
+    "use strict";
 
-  return Controller.extend("ui5.walkthrough.controller.App", {
-    onShowHello() {
-      // show a native JavaScript alert
-      alert("Hello World");
-    },
-  });
-});
+    return Controller.extend("ui5.walkthrough.controller.App", {
+      onShowHello() {
+        MessageToast.show("Hello World");
+      },
+    });
+  }
+);
